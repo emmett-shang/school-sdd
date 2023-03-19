ddaaf670aa6b0df654c0228a626fc862c23b4854
emmett-shang <emmett.shang@gmail.com>
2023-03-19 13:35:43 +1100

    Added functions for each size category and grade and switched if statements to match case

--- jacket_assessment_v2.py
+++ jacket_assessment_v2.py
@@ -2,7 +2,59 @@ import random  # import the libary, random
 
 import time  # import the library, time
 
+
+
+
 clothes_list = []  # placeholder/empty list for later use
+
+
+def rejected():
+    category = "rejected"
+    graded = "N/A"
+
+    return category, graded
+
+
+def small(grade):
+    graded = ""
+
+    category = "small"
+
+    if grade.lower() == "j":  # junior
+
+        graded = "junior"
+
+    elif grade.lower() == "s":  # senior
+        graded = "senior"
+
+    return category, graded
+
+
+def medium(grade):
+    graded = ""
+
+    category = "medium"
+    if grade.lower() == "j":  # junior
+
+        graded = "junior"
+
+    elif grade.lower() == "s":  # senior
+        graded = "senior"
+
+    return category, graded
+
+
+def large(grade):
+    graded = ""
+
+    category = "large"
+    if grade.lower() == "j":  # junior
+
+        graded = "junior"
+
+    elif grade.lower() == "s":  # senior
+        graded = "senior"
+    return category, graded
 
 
 def jacket_sorter_manual():  # clothes_sorter function
@@ -14,40 +66,38 @@ def jacket_sorter_manual():  # clothes_sorter function
             size = int(size)  # converts size into integer
             grade = input("Junior(j) or Senior(s)? ")  # ask for the grade of clothes, whether its junior or senior
             if grade.lower() == "j" or grade.lower() == "s":  # checks for valid input
-                if size < 5:  # rejected
-                    category = "rejected"
-                    print(category)
-                    grade = "N/A"
-                    print("grade: " + grade)
-                    print("-------------------------------")
-                elif 5 <= size <= 10:  # small
-                    print("accepted")
-                    category = "small"
-                    print("size: " + category)
+                match size:
+                    case _ if size < 5:
+                        print("size and grade: " + str(rejected()))  # rejected
+                        clothes_final = (
+                            size, *rejected()
+                        )  # adds all the values including size, category and grade into a variable
+                        clothes_list.append(clothes_final)
+                        print("-------------------------------")
 
-                    if grade.lower() == "j":  # junior
+                    case _ if 5 <= size <= 10:
+                        print("size and grade: " + str(small(grade)))
+                        clothes_final = (
+                            size, *small(grade)
+                        )  # adds all the values including size, category and grade into a variable
+                        clothes_list.append(clothes_final)
+                        print("-------------------------------")
 
-                        grade = "junior"
-                        print("grade: " + grade)
-                    elif grade.lower() == "s":  # senior
-                        grade = "senior"
-                        print("grade: " + grade)
+                    case _ if 10 < size <= 20:
+                        print("size and grade: " + str(medium(grade)))
+                        clothes_final = (
+                            size, *medium(grade))  # adds all the values including size, category and grade into a variable
+                        clothes_list.append(clothes_final)
+                        print("-------------------------------")
 
-                    print("-------------------------------")
-                elif 10 < size <= 20:  # medium
-                    print("accepted")
-                    category = "medium"
-                    print("size: " + category)
-                else:  # large
-                    print("accepted")
-                    category = "large"
-                    print("size: " + category)
+                    case _ if size > 20:
+                        print("size and grade: " + str(large(grade)))
+                        clothes_final = (
+                            size, *large(grade))  # adds all the values including size, category and grade into a variable
+                        clothes_list.append(clothes_final)
+                        print("-------------------------------")
 
-                    print("-------------------------------")
-
-                clothes_final = (
-                    size, category, grade)  # adds all the values including size, category and grade into a variable
-                clothes_list.append(clothes_final)  # adds clothes_final values to the list clothes_list
+                # adds clothes_final values to the list clothes_list
             else:  # returns a message for invalid input of grade
                 print("This is not a valid grade")
                 continue  # skips to the next iteration of the loop
@@ -66,60 +116,86 @@ def jacket_sorter_manual():  # clothes_sorter function
             break
 
 
+def rejected_auto():
+    category = "rejected"
+    grade = "N/A"
+
+    return category, grade
+def small_auto():
+
+    category = "small"
+
+    if random.random() <= 0.75:  # returns a random value between 0 and 1. If the value is below 0.75 grade = Junior making it a 75% chance
+        grade = "junior"
+
+    else:  # else returns senior grade
+        grade = "senior"
+
+    return category, grade
+
+def medium_auto():
+    category = "medium"
+    if random.random() <= 0.5:  # if the value is below 0.5, grade = junior (50% chance)
+        grade = "junior"
+
+    else:  # else grade = senior
+        grade = "senior"
+
+    return category, grade
+
+def large_auto():
+
+    category = "large"
+
+    if random.random() <= 0.25:  # if the value is below 0.25, grade = junior (25% chance)
+        grade = "junior"
+
+    else:  # else grade = senior
+        grade = "senior"
+
+    return category, grade
+
 def jacket_sorter_auto():
     grade = ""
     num = input("How many clothes would you like to sort: ")  # retrives input for number of clothes you want to input
+
     if num.isdigit():  # checks if "num" can be converted to an integer
         for i in range(int(num)):  # loop "num" amount of times
             size_auto = random.randint(0, 30)  # generate a random clothes size between 0 and 30
-            print(f"Weight of clothes: {size_auto}")
-            if size_auto < 5:  # rejected
-                category = "rejected"
-                print(category)
-                print("-------------------------------")
 
-                time.sleep(1)  # delay for 1 second
-            elif size_auto >= 5 and size_auto <= 10:  # small
-                print("accepted")
-                category = "small"
-                print("size: " + category)
-                if random.random() <= 0.75:  # returns a random value between 0 and 1. If the value is below 0.75 grade = Junior making it a 75% chance
-                    grade = "junior"
-                    print("grade: " + grade)
-                else:  # else returns senior grade
-                    grade = "senior"
-                    print("grade: " + grade)
-                print("-------------------------------")
+            match size_auto:
 
-                time.sleep(1)
-            elif size_auto > 10 and size_auto <= 20:  # medium
-                print("accepted")
-                category = "medium"
-                print("size: " + category)
-                if random.random() <= 0.5:  # if the value is below 0.5, grade = junior (50% chance)
-                    grade = "junior"
-                    print("grade: " + grade)
-                else:  # else grade = senior
-                    grade = "senior"
-                    print("grade: " + grade)
-                print("-------------------------------")
+                case _ if size_auto < 5:
+                    print(f"Weight of clothes: {size_auto}")
+                    print("size and grade: " + str(rejected_auto()))
+                    print("-------------------------------")
+                    clothes_final = (size_auto, *rejected_auto())  # assigns or the values including size of clothes, category and grade to a variable
+                    clothes_list.append(clothes_final)
 
-                time.sleep(1)
-            else:  # large
-                print("accepted")
-                category = "large"
-                print("size: " + category)
-                if random.random() <= 0.25:  # if the value is below 0.25, grade = junior (25% chance)
-                    grade = "junior"
-                    print("grade: " + grade)
-                else:  # else grade = senior
-                    grade = "senior"
-                    print("grade: " + grade)
-                print("-------------------------------")
-                time.sleep(1)
-            clothes_final = (size_auto, category,
-                             grade)  # assigns or the values including size of clothes, category and grade to a variable
-            clothes_list.append(clothes_final)  # adds clothes_final values to the list clothes_list
+                    time.sleep(1)  # delay for 1 second
+                case _ if 5 <= size_auto <= 10:
+                    print(f"Weight of clothes: {size_auto}")
+                    print("size and grade: " + str(small_auto()))
+                    print("-------------------------------")
+                    clothes_final = (size_auto, *small_auto())  # assigns or the values including size of clothes, category and grade to a variable
+                    clothes_list.append(clothes_final)
+                    time.sleep(1)
+                case _ if 10 < size_auto <= 20:  # medium
+                    print(f"Weight of clothes: {size_auto}")
+                    print("size and grade: " + str(medium_auto()))
+                    print("-------------------------------")
+                    clothes_final = (size_auto, *medium_auto())  # assigns or the values including size of clothes, category and grade to a variable
+                    clothes_list.append(clothes_final)
+                    time.sleep(1)
+                case _ if size_auto < 20:  # large
+                    print(f"Weight of clothes: {size_auto}")
+                    print("size and grade: " + str(large_auto()))
+                    clothes_final = (size_auto, *large_auto())  # assigns or the values including size of clothes, category and grade to a variable
+                    clothes_list.append(clothes_final)
+                    print("-------------------------------")
+                    time.sleep(1)
+
+              # adds clothes_final values to the list clothes_list
     else:  # if num cannot be converted to an integer return error message
         print("Invalid input")
         pass  # loop continues to the next step
@@ -129,18 +205,23 @@ def repeat_sorter():
     while True:
         mode = input(
             "do you want manual(m) or automatic(a) mode or are you done(d): ")  # input for the mode option either manual mode, automatic mode or exiting and ending the program
-        if mode.lower() == "m":  # checks if the mode is manual (keyword it is looking for is m). .lower() makes it so that it is not case sensitive
-            jacket_sorter_manual()
-        elif mode.lower() == "a":  # else if the mode input is "a" or "A"
-            jacket_sorter_auto()
-        elif mode.lower() == "d":  # if mode input = "d" or "D"
-            print("clothes sorter shutting down...")
-            print("-------------------------------")
-            break
+        match mode:
 
-        else:  # else if the mode input is none of the above return an error message
-            print("Input is invalid. Please input a valid option")
-            print("-------------------------------")
+            case _ if mode.lower() == "m":  # checks if the mode is manual (keyword it is looking for is m). .lower() makes it so that it is not case sensitive
+                jacket_sorter_manual()
+            case _ if mode.lower() == "a":  # else if the mode input is "a" or "A"
+                jacket_sorter_auto()
+            case _ if mode.lower() == "d":  # if mode input = "d" or "D"
+                print("clothes sorter shutting down...")
+                print("-------------------------------")
+                break
+
+            case _:  # else if the mode input is none of the above return an error message
+                print("Input is invalid. Please input a valid option")
+                print("-------------------------------")
+
+    if len(clothes_list) == 0:
+        print("Why did you even turn on the sorter?")
 
     print("All the sorted clothes in your session: \n" + str(clothes_list))
 
